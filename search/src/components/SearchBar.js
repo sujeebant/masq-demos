07757a6b0f4a9a053a075fad0706baf4788d9ca8
G-Ray <g.bonneville@qwant.com>
2018-04-16 17:32:43 +0200

    Open Qwant with the current query

--- search/src/components/SearchBar.js
+++ search/src/components/SearchBar.js
@@ -44,47 +44,72 @@ const styles = {
   }
 }
 
-export default function SearchBar (props) {
-  return (
-    <Downshift
-      render={({
-        getInputProps,
-        getItemProps,
-        isOpen,
-        inputValue,
-        highlightedIndex,
-        selectedItem
-      }) => (
-        <div style={styles.SearchBar}>
-          <div>
-            <input {...getInputProps()} style={styles.input} placeholder='What are you looking for?' />
-            <Search style={styles.icon} />
-          </div>
+export default class SearchBar extends React.Component {
+  constructor (props) {
+    super(props)
+    this.state = { query: '' }
+    this.openQwant = this.openQwant.bind(this)
+    this.handleKeyPress = this.handleKeyPress.bind(this)
+  }
 
-          {isOpen &&
-            <div style={styles.downshift}>
-              {items
-                .filter(i => !inputValue || i.includes(inputValue))
-                .map((item, index) => (
-                  <div
-                    {...getItemProps({
-                      key: item,
-                      index,
-                      item,
-                      style: Object.assign({}, styles.downshiftRow, {
-                        backgroundColor:
-                          highlightedIndex === index ? 'lightgray' : 'white',
-                        fontWeight: selectedItem === item ? 'bold' : 'normal'
-                      })
-                    })}
-                  >
-                    {item}
-                  </div>
-                ))}
+  openQwant () {
+    window.open('https://www.qwant.com/?q=' + this.state.query)
+  }
+
+  handleKeyPress (e) {
+    if (e.key === 'Enter') {
+      this.openQwant()
+    }
+  }
+
+  render () {
+    return (
+      <Downshift
+        onStateChange={({ inputValue }) => {
+          return inputValue && this.setState({ query: inputValue })
+        }}
+        selectedItem={this.state.query}
+        render={({
+          getInputProps,
+          getItemProps,
+          isOpen,
+          inputValue,
+          highlightedIndex,
+          selectedItem
+        }) => (
+          <div style={styles.SearchBar}>
+            <div>
+              <input {...getInputProps({
+                onKeyUp: this.handleKeyPress
+              })} style={styles.input} placeholder='What are you looking for?' />
+              <Search onClick={this.openQwant} style={styles.icon} />
             </div>
-          }
-        </div>
-      )}
-    />
-  )
+
+            {isOpen &&
+              <div style={styles.downshift}>
+                {items
+                  .filter(i => !inputValue || i.includes(inputValue))
+                  .map((item, index) => (
+                    <div
+                      {...getItemProps({
+                        key: item,
+                        index,
+                        item,
+                        style: Object.assign({}, styles.downshiftRow, {
+                          backgroundColor:
+                            highlightedIndex === index ? 'lightgray' : 'white',
+                          fontWeight: selectedItem === item ? 'bold' : 'normal'
+                        })
+                      })}
+                    >
+                      {item}
+                    </div>
+                  ))}
+              </div>
+            }
+          </div>
+        )}
+      />
+    )
+  }
 }
