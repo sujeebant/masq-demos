4b7e214690d5a5cd2c18a3971221bab89a4e60e0
G-Ray <g.bonneville@qwant.com>
2018-04-17 17:07:34 +0200

    Clean unused code

--- search/src/App.js
+++ search/src/App.js
@@ -7,9 +7,6 @@ class App extends Component {
   render () {
     return (
       <div className='App'>
-        {/* <header className='App-header'>
-          <h1 className='App-title'>Masq Demo - Search</h1>
-        </header> */}
         <SearchBar />
       </div>
     )
