84435c782c0453c10c0ea89e883363e036d92e2a
G-Ray <g.bonneville@qwant.com>
2018-04-30 15:54:53 +0200

    Use client lib to addApp and set a test key

--- search/src/App.js
+++ search/src/App.js
@@ -3,9 +3,30 @@ import './App.css'
 
 import SearchBar from './components/SearchBar'
 
+import { Client } from './masq/client'
+
 class App extends Component {
   onSearch (query) {
     console.log('query:', query)
+  }
+
+  async componentDidMount () {
+    const appInfo = {
+      url: 'https://masq.io/search',
+      name: 'Masq Search',
+      description: 'Masq Search'
+    }
+    try {
+      const client = new Client({
+        socketUrl: 'ws://localhost:8080',
+        socketConf: { requestTimeout: 0 }
+      })
+      await client.initWS()
+      await client.addApp(appInfo)
+      await client.setItem('key', 'value')
+    } catch (err) {
+      console.error(err)
+    }
   }
 
   render () {
