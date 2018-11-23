27af25bda7c7e4377d219af7b05c2b16a5f34361
G-Ray <g.bonneville@qwant.com>
2018-11-23 15:11:18 +0100

    Update Search app to use masq-lib api

---
+++ search/src/App.jsx
@@ -0,0 +1,110 @@
+import React, { Component } from 'react'
+import Masq from 'masq-lib'
+
+import SearchBar from './components/SearchBar'
+import Profiles from './components/Profiles'
+
+import './App.css'
+
+const APP = {
+  name: 'Qwant Search',
+  description: 'Qwant search engine',
+  image: 'https://qwant.com/img/v4/home-banner.svg?1542966874062'
+}
+
+// function ConnectionStatus ({ isConnected }) {
+//   return isConnected
+//     ? <p style={{ color: 'green' }}>Connected to Masq</p>
+//     : <p style={{ color: 'red' }}>Disconnected from Masq</p>
+// }
+
+class App extends Component {
+  constructor (props) {
+    super(props)
+
+    this.state = {
+      link: '#',
+      items: [],
+      profiles: []
+    }
+
+    this.masq = null
+    this.onSearch = this.onSearch.bind(this)
+    this.handleClickProfile = this.handleClickProfile.bind(this)
+  }
+
+  async componentDidMount () {
+    this.masq = new Masq('Search')
+    await this.masq.init()
+
+    try {
+      const profiles = await this.masq.getProfiles()
+      this.setState({ profiles })
+    } catch (e) {
+      // We need to link with masq for the first time
+
+      // Retrieve link to request an access to Masq
+      const { link } = await this.masq.requestMasqAccess()
+      this.setState({ link })
+      // Waith for the operation to complete
+      await this.masq.requestMasqAccessDone()
+
+      // We can now retrieve the profiles
+      const profiles = await this.masq.getProfiles()
+      this.setState({ profiles })
+    }
+  }
+
+  async onSearch (query) {
+    const items = [...this.state.items, query]
+
+    try {
+      // We create one key in DB per search item instead
+      // of updating an array
+      await this.masq.put(query, 'ok')
+      this.setState({ items })
+    } catch (e) {
+      console.error(e.message)
+    }
+  }
+
+  async handleClickProfile (profile) {
+    let db = null
+    this.setState({ currentProfile: profile })
+
+    await this.masq.setProfile(profile.id)
+
+    try {
+      db = this.masq._getDB()
+    } catch (e) {
+      // Retrieve link to create the app
+      const { link } = await this.masq.exchangeDataHyperdbKeys(APP)
+      window.open(link, '_blank')
+      // Wait for the operation to complete
+      await this.masq.exchangeDataHyperdbKeysDone(APP)
+      db = this.masq._getDB()
+    }
+
+    // Retrieve existing keys, in order to
+    // retrieve items from the values stored in the DB
+    db.list('', (err, list) => {
+      if (err) console.error(err)
+      const items = list.map(nodes => nodes[0].key)
+      this.setState({ items })
+    })
+  }
+
+  render () {
+    const { profiles, items, currentProfile, link } = this.state
+    return (
+      <div className='App'>
+        {!profiles.length && <a target='_blank' rel="noopener noreferrer" href={link}>connect to masq</a>}
+        <Profiles profiles={profiles} onClick={this.handleClickProfile} />
+        {currentProfile && <p>Selected user: {currentProfile.username}</p>}
+        {currentProfile && <SearchBar onSearch={this.onSearch} items={items} />}
+      </div>
+    )
+  }
+}
+
+export default App
--- search/src/components/SearchBar.jsx
+++ search/src/components/SearchBar.jsx
@@ -3,45 +3,6 @@ import Downshift from 'downshift'
 
 import Search from '../icons/Search'
 
-const styles = {
-  SearchBar: {
-    position: 'absolute',
-    top: '50%',
-    width: 480,
-    height: 56,
-    left: '50%',
-    marginLeft: -240,
-    marginTop: -56,
-    borderBottom: '2px solid #747474'
-  },
-  input: {
-    width: 384,
-    height: 32,
-    fontSize: 28,
-    border: 0,
-    outline: 0,
-    float: 'left',
-    color: '#747474'
-  },
-  icon: {
-    width: 42,
-    height: 42,
-    float: 'right',
-    color: '#747474',
-    cursor: 'pointer'
-  },
-  downshift: {
-    marginTop: 58,
-    textAlign: 'left',
-    cursor: 'pointer',
-    lineHeight: '32px',
-    boxShadow: 'grey 3px 5px 9px 2px'
-  },
-  downshiftRow: {
-    fontSize: 20
-  }
-}
-
 export default class SearchBar extends React.Component {
   constructor (props) {
     super(props)
@@ -85,29 +46,29 @@ export default class SearchBar extends React.Component {
           highlightedIndex,
           selectedItem
         }) => (
-          <div style={styles.SearchBar}>
+          <div className='SearchBar'>
             <div>
               <input {...getInputProps({
                 onKeyUp: this.handleKeyPress
-              })} style={styles.input} ref={this.inputRef} placeholder='What are you looking for?' />
-              <Search onClick={this.openQwant} style={styles.icon} />
+              })} className='input' ref={this.inputRef} placeholder='What are you looking for?' />
+              <Search onClick={this.openQwant} className='icon' />
             </div>
 
             {isOpen &&
-              <div style={styles.downshift}>
+              <div className='downshift'>
                 {this.props.items
                   .filter(i => !inputValue || i.includes(inputValue))
                   .map((item, index) => (
                     <div
+                      className='downshiftRow'
                       {...getItemProps({
                         key: index,
                         index,
                         item,
-                        style: Object.assign({}, styles.downshiftRow, {
-                          backgroundColor:
-                            highlightedIndex === index ? 'lightgray' : 'white',
+                        style: {
+                          backgroundColor: highlightedIndex === index ? 'lightgray' : 'white',
                           fontWeight: selectedItem === item ? 'bold' : 'normal'
-                        })
+                        }
                       })}
                     >
                       {item}
