f5759d4ca6cc9b0e2d23769b033d3356e3b37c7f
G-Ray <g.bonneville@qwant.com>
2018-12-11 17:26:55 +0100

    Update with new api

--- search/src/App.jsx
+++ search/src/App.jsx
@@ -26,12 +26,12 @@ class App extends Component {
       items: [],
       loggedIn: null,
       err: null,
-      logginIn: false
     }
 
     this.masq = null
     this.onSearch = this.onSearch.bind(this)
     this.handleClickLogin = this.handleClickLogin.bind(this)
+    this.handleClickLogout = this.handleClickLogout.bind(this)
   }
 
   async componentDidMount () {
@@ -41,8 +41,16 @@ class App extends Component {
       try {
         await this.masq.connectToMasq()
         this.setState({ loggedIn })
-        const items = await this.masq.get('queryList')
-        this.setState({ items })
+        // const items = await this.masq.get('queryList')
+        const db = this.masq._getDB()
+
+        // Retrieve existing keys, in order to
+        // retrieve items from the values stored in the DB
+        db.list((err, list) => {
+          if (err) console.error(err)
+          const items = list.map(nodes => nodes[0].key)
+          this.setState({ items })
+        })
       } catch (err) {
         this.setState({ err })
       }
@@ -51,35 +59,48 @@ class App extends Component {
 
   async onSearch (query) {
     const items = [...this.state.items, query]
-    this.setState({ items })
-
-    if (this.masq.isConnected()) {
-      this.syncSearchItemsWithMasq()
-    }
-  }
-
-  async syncSearchItemsWithMasq () {
     try {
       // We create one key in DB per search item instead
       // of updating an array
-      await this.masq.put('queryList', this.state.items)
+      await this.masq.put(query, 'ok')
+      this.setState({ items })
     } catch (e) {
       console.error(e.message)
     }
   }
 
   async handleClickLogin () {
-    this.setState({ logginIn: true })
-    const { link } = await this.masq.logIntoMasq(true)
-    window.open(link, '_blank')
-    this.setState({ logginIn: false, items: this.state.items })
+    try {
+      const { link } = await this.masq.logIntoMasq(true)
+      window.open(link, '_blank')
+      await this.masq.logIntoMasqDone()
+      this.setState({
+        loggedIn: true
+      })
+    } catch (e) {
+      console.error(e)
+    }
+  }
+
+  async handleClickLogout () {
+    this.setState({ loggedIn: false })
+    await this.masq.signout()
   }
 
   render () {
     const { items, loggedIn } = this.state
     return (
       <div className='App'>
-        {!loggedIn && <input type="button" target='_blank' rel="noopener noreferrer" onClick={this.handleClickLogin} value="Log Into Masq"></input>}
+        {!loggedIn && (
+          <button
+            type="button"
+            target='_blank'
+            rel="noopener noreferrer"
+            onClick={this.handleClickLogin}
+          >Log Into Masq
+          </button>
+        )}
+        {loggedIn && <button onClick={this.handleClickLogout}>Logout</button>}
         <SearchBar onSearch={this.onSearch} items={items} />
       </div>
     )
