9efad9d8ebfc9a08f18f0ca6c733fb0cd0fbe18f
G-Ray <g.bonneville@qwant.com>
2018-12-12 10:56:43 +0100

    remove first loggedIn state

--- search/src/App.jsx
+++ search/src/App.jsx
@@ -54,7 +54,6 @@ class App extends Component {
     try {
       const { link } = await this.masq.logIntoMasq(true)
       this.setState({ link })
-      this.setState({ loggedIn: true })
       this.getAllQueriesFromDB()
     } catch (err) {
       this.setState({ err })
